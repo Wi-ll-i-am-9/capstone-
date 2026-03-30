22ac956ae8d627f7ad31a56cca478b5b3091e9a6
Wi-ll-i-am-9 <wsenou5@gmail.com>
2026-03-30 16:46:46 +0000

    WORKS ABIT

--- main.sql
+++ main.sql
@@ -5,12 +5,12 @@ SALESMAN_NAME TEXT,
 CITY TEXT,
 COMMISION TEXT
 );
-INSERT INTO Salesman(SALESMAN_ID,SALESMAN_NAME,CITY,COMMISION) VALUES
-(42463,"Daniel Lorenzo","Rome","0.10"),
-(25334,"Daniel Lorenzo","Madrid","0.11"),
-(253,"Killian Jean-Paul","Paris","0.17"),
-(676766,"Connor Withaker","Oslo","0.23"),
-(38474,"Bob Garl","Chicago","0.24");
+INSERT INTO Salesman(SALESMAN_ID, SALESMAN_NAME, CITY, COMMISION) VALUES
+(42463,'Daniel Lorenzo','Rome','0.10'),
+(25334,'Dan Lorenzo','Madrid','0.11'),
+(253,'Killian Jean-Paul','Paris','0.17'),
+(676766,'Connor Withaker','Oslo','0.23'),
+(38474,'Bob Garl','Chicago','0.24');
 DROP TABLE IF EXISTS Customers;
 CREATE TABLE Customers(
 CUSTOMER_ID INT,
@@ -18,18 +18,25 @@ CUSTOMER_NAME TEXT,
 CUSTOMER_CITY TEXT,
 SALESMAN_ID INT
 );
-INSERT INTO Customers(CUSTOMER_ID,CUSTOMER_NAME,CUSTOMER_CITY,SALESMAN_ID) VALUES
-(25373,"Gigi Rex","Rome",42463),
-(424633,"Gianluigi DiGiovani","Rome",42463),
-(424635,"Francesca DiGiovani","Rome",42463),
-(44523,"Elona Varbas","Madrid",25334),
-(123454,"Carl Jean-Luc François","Paris",253),
-(9,"Erling Haaland","Oslo",676766),
-(10,"Alexander Sørloth","Oslo",676766),
-(8,"Martin Øedegard","Oslo",676766),
-(42,"Dwayne Johnson","Chicago",38474),
-(6000,"Jimmy Donaldson","Chicago",38474),
-(50,"Curtis Jackson","Chicago",38474),
-(1,"Donald Trump","Chicago",38474);
+INSERT INTO Customers(CUSTOMER_ID, CUSTOMER_NAME, CUSTOMER_CITY, SALESMAN_ID) VALUES
+(25373,'Gigi Rex','Rome',42463),
+(424633,'Gianluigi DiGiovani','Rome',42463),
+(424635,'Francesca DiGiovani','Rome',42463),
+(44523,'Elona Varbas','Madrid',25334),
+(123454,'Carl Jean-Luc François','Paris',253),
+(9,'Erling Haaland','Oslo',676766),
+(10,'Alexander Sørloth','Oslo',676766),
+(8,'Martin Øedegard','Oslo',676766),
+(42,'Dwayne Johnson','Chicago',38474),
+(6000,'Jimmy Donaldson','Chicago',38474),
+(50,'Curtis Jackson','Chicago',38474),
+(1,'Donald Trump','Chicago',38474);
 SELECT * FROM Salesman;
 SELECT * FROM Customers;
+--SELECT * FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID;
+SELECT SALESMAN_NAME,CUSTOMER_NAME FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID;
+SELECT SALESMAN_NAME,CUSTOMER_NAME,CUSTOMER_CITY,CITY FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID WHERE CUSTOMER_CITY='Oslo';
+SELECT SALESMAN_NAME,CUSTOMER_NAME,CITY FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID WHERE CITY='Rome';
+SELECT SALESMAN_NAME,COUNT(CUSTOMER_ID) AS TOTAL_CUSTOMERS FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID GROUP BY SALESMAN_NAME;
+SELECT CITY,COUNT(CUSTOMER_ID) AS TOTAL_CUSTOMERS FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID GROUP BY CITY;
+SELECT CITY, CUSTOMER_CITY FROM Salesman JOIN Customers ON Salesman.SALESMAN_ID=Customers.SALESMAN_ID;
